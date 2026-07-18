cdc436b21849739163e634ed3133cf632e53e019
Andy <topnights@sky.com>
2026-07-18 17:03:20 +0100

    Add Vercel Analytics

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,9 +1,9 @@
 export const TOP_EURO_LEAGUES = [
-  { id: 39, name: "Premier League", country: "England", season: 2025 },
-  { id: 140, name: "La Liga", country: "Spain", season: 2025 },
-  { id: 135, name: "Serie A", country: "Italy", season: 2025 },
-  { id: 78, name: "Bundesliga", country: "Germany", season: 2025 },
-  { id: 61, name: "Ligue 1", country: "France", season: 2025 },
-  { id: 88, name: "Eredivisie", country: "Netherlands", season: 2025 },
-  { id: 94, name: "Primeira Liga", country: "Portugal", season: 2025 },
+  { id: 39, name: "Premier League", country: "England", season: 2026 },
+  { id: 140, name: "La Liga", country: "Spain", season: 2026 },
+  { id: 135, name: "Serie A", country: "Italy", season: 2026 },
+  { id: 78, name: "Bundesliga", country: "Germany", season: 2026 },
+  { id: 61, name: "Ligue 1", country: "France", season: 2026 },
+  { id: 88, name: "Eredivisie", country: "Netherlands", season: 2026 },
+  { id: 94, name: "Primeira Liga", country: "Portugal", season: 2026 },
 ] as const;
